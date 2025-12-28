464e01a12008e99f33ac1dd45440992703a9591c
YoungApedev <olumidestephen503@gmail.com>
2025-12-28 17:02:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>T T Couture</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1,user-scaleable=no">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+  </script>
+  <script src="js/bootstrap.main.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a href="/tutorial/index.php" class="navbar-brand">T T Couture</a>
+    <ul class="nav navbar-nav">
+<!--Drop Down Menu-->
+    <li class="dropdown">
+<a href="*" class="dropdown-toggle" data-toggle="dropdown" id="text">Women's<span class="caret></span>"<a/>
+  <ul class="dropdown-menu" role="menu"
+  <li><a href="*">Ready to wear</a></li>
+    <li><a href="*">Custom made</a></li>
+    <li><a href="*">Jumpsuits</a></li>
+    <li><a href="*">Accesories</a></li>
+  </ul>
+</li>
+</ul>
+  </div>
+</nav>
+<!-- Inserting images -->
+<div id="background-image">
+    <div id="image-1"></div>
+    <div id="image-2"></div>
+</div>
+<p>
+  Lorem ipsum has been widely used in Western printing and design since the 15th century. With the rise of computer typesetting, this seemingly meaningless text, used by the traditional printing industry for hundreds of years, has become popular again. Because this text begins with "Lorem ipsum" and is often used in headline tests, it is generally called Lorem ipsum, or simply Lipsum.
+Common Lorem ipsum prefixes are as follows:
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Exceptional sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Initially, it was thought that this Latin phrase was merely a meaningless combination, intended to distract the reader from the text's content and focus solely on the letterforms or layout, thus filling in the space. However, according to research by American Latin scholar Richard McClintock, Lorem ipsum actually originates from Cicero's *De finibus bonorum et malorum* (The End of Good and Evil).
+Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…
+(Translation: No one loves suffering, nor does anyone seek or desire it, because it is suffering...)
+To improve readability and make the frequency of letters more similar to modern English, some versions have replaced certain letters with letters not found in Latin, such as K, W, and Z, or added words like zzril and takimata. Besides the original Lorem ipsum, several new versions of Lorem ipsum exist.
+</body>
+</html>
